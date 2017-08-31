62cfe9ff831b8fe8027db44cc18f004c7d10820c
joyce <joyce-adri@users.noreply.github.com>
2017-08-31 12:44:20 -0500

    archivos base
    archivos: esquema examen

---
+++ Version_B/test_B.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import unittest
+import tema1
+import tema2
+
+
+class Test(unittest.TestCase):
+    
+# >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
+# Reemplazar EL ID por el numero correspondiente del caso de prueba
+    
+    def test_tema1_prueba_ID(self):
+        # inserte su codigo de prueba
+        self.assertEquals()
+
+    def test_tema2_prueba_ID(self):
+        # inserte su codigo de prueba
+        self.assertEquals()
+
+if __name__ == '__main__':
+    unittest.main()

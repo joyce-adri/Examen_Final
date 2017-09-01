7e54be4f69746c4574c95ccc46c77400f635b9a7
joyce-adri <joyce01sm@gmail.com>
2017-09-01 18:02:18 -0500

    tema 2 caso_prueba_1

--- Version_B/test_B.py
+++ Version_B/test_B.py
@@ -81,18 +81,6 @@ class Test(unittest.TestCase):
 		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
 		self.assertEqual(tarjeta,"Tipo de tarjeta: Gold")
 
-    def test_tema2_prueba_2(self):
-		tipoCliente = 'AAA'
-		montoDeuda = 19999
-		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
-		self.assertEqual(tarjeta,"Tipo de tarjeta: Clasica")
-        
-    def test_tema2_prueba_3(self):
-		tipoCliente = 'AA'
-		montoDeuda = 15000
-		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
-		self.assertEqual(tarjeta,"Tipo de tarjeta: Platinum")   
- 
         
 if __name__ == '__main__':
     unittest.main()

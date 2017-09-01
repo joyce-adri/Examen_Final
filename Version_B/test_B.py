8dffcbde59c763866227291fcafd196a6ac799fe
LuisBSC15 <luallain@hotmail.com>
2017-09-01 10:08:59 -0500

    Update test_B.py
    tema 1 y tema 2

--- Version_B/test_B.py
+++ Version_B/test_B.py
@@ -70,13 +70,81 @@ class Test(unittest.TestCase):
         montoInversion10 = 9000
         msg = premio_por_deposito_a_plazo_fijo(plazo10, montoInversion10)
         self.assertEquals(msg,"no recibe premio")
+        
+    def test_tema1_prueba_11(self):
+        plazo11 = 180
+        montoInversion11 = 9000
+        msg = premio_por_deposito_a_plazo_fijo(plazo11, montoInversion11)
+        self.assertEquals(msg,"no recibe premio")
+        
+    def test_tema1_prueba_12(self):
+        plazo12 = 360
+        montoInversion12 = 9000
+        msg = premio_por_deposito_a_plazo_fijo(plazo12, montoInversion12)
+        self.assertEquals(msg,"no recibe premio")
 
         
         
 
-    def test_tema2_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
+    def test_tema2_prueba_1(self):
+        tipoCliente = "AAA"
+        montoEndeudamiento = 20000
+        msg = ofrecer_tarjeta_con_chip(montoEndeudamiento, tipoCliente)
+        self.assertEquals(msg,"Gold")
+
+    def test_tema2_prueba_2(self):
+        tipoCliente = "AA"
+        montoEndeudamiento = 17000
+        msg = ofrecer_tarjeta_con_chip(montoEndeudamiento, tipoCliente)
+        self.assertEquals(msg,"Platinum")
+
+    def test_tema2_prueba_3(self):
+        tipoCliente = "A"
+        montoEndeudamiento = 13000
+        msg = ofrecer_tarjeta_con_chip(montoEndeudamiento, tipoCliente)
+        self.assertEquals(msg,"Internacional")
+
+    def test_tema2_prueba_4(self):
+        tipoCliente = "B"
+        montoEndeudamiento = 7000
+        msg = ofrecer_tarjeta_con_chip(montoEndeudamiento, tipoCliente)
+        self.assertEquals(msg,"Club Miles")
+
+    def test_tema2_prueba_5(self):
+        tipoCliente = "AAA"
+        montoEndeudamiento = 4000
+        msg = ofrecer_tarjeta_con_chip(montoEndeudamiento, tipoCliente)
+        self.assertEquals(msg,"Advantage")
+
+    def test_tema2_prueba_6(self):
+        tipoCliente = "AAA"
+        montoEndeudamiento = 19000
+        msg = ofrecer_tarjeta_con_chip(montoEndeudamiento, tipoCliente)
+        self.assertEquals(msg,"Clasica")
+
+    def test_tema2_prueba_7(self):
+        tipoCliente = "AA"
+        montoEndeudamiento = 10000
+        msg = ofrecer_tarjeta_con_chip(montoEndeudamiento, tipoCliente)
+        self.assertEquals(msg,"Clasica")
+
+    def test_tema2_prueba_8(self):
+        tipoCliente = "A"
+        montoEndeudamiento = 9000
+        msg = ofrecer_tarjeta_con_chip(montoEndeudamiento, tipoCliente)
+        self.assertEquals(msg,"Clasica"
+
+    def test_tema2_prueba_9(self):
+        tipoCliente = "B"
+        montoEndeudamiento = 4000
+        msg = ofrecer_tarjeta_con_chip(montoEndeudamiento, tipoCliente)
+        self.assertEquals(msg,"Clasica")
+
+    def test_tema2_prueba_10(self):
+        tipoCliente = "C"
+        montoEndeudamiento = 2000
+        msg = ofrecer_tarjeta_con_chip(montoEndeudamiento, tipoCliente)
+        self.assertEquals(msg,"Clasica")
 
 if __name__ == '__main__':
     unittest.main()

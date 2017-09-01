7e3f11036b0d8598b5ea3c8653c7a9b8a7094069
Ivan Mera <ivan@cajanegra.com.ec>
2017-09-01 09:07:52 -0500

    Pruebas tema 1

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -6,17 +6,45 @@ import tema1
 import tema2
 
 class Test(unittest.TestCase):
-    
+	
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_tema1_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
+	
+	def test_tema1_prueba_1(self):
+		result = tema1.etiquetado_consumo_energia(20)
+		self.assertEquals(result, "escala: A++ , eficiencia: los más eficientes", "failure")
 
-    def test_tema2_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
+	def test_tema1_prueba_2(self):
+		result = tema1.etiquetado_consumo_energia(40)
+		self.assertEquals(result, "escala: A+ , eficiencia: los más eficientes", "failure")
+
+	def test_tema1_prueba_3(self):
+		result = tema1.etiquetado_consumo_energia(50)
+		self.assertEquals(result, "escala: A , eficiencia: los más eficientes", "failure")
+
+	def test_tema1_prueba_4(self):
+		result = tema1.etiquetado_consumo_energia(60)
+		self.assertEquals(result, "escala: B , eficiencia: los más eficientes", "failure")
+
+	def test_tema1_prueba_5(self):
+		result = tema1.etiquetado_consumo_energia(80)
+		self.assertEquals(result, "escala: C , eficiencia: los más eficientes", "failure")
+
+	def test_tema1_prueba_6(self):
+		result = tema1.etiquetado_consumo_energia(95)
+		self.assertEquals(result, "escala: D , eficiencia: consumo medio", "failure")
+
+	def test_tema1_prueba_7(self):
+		result = tema1.etiquetado_consumo_energia(105)
+		self.assertEquals(result, "escala: E , eficiencia: consumo medio", "failure")
+
+	def test_tema1_prueba_8(self):
+		result = tema1.etiquetado_consumo_energia(110)
+		self.assertEquals(result, "escala: F , eficiencia: alto consumo energético", "failure")
+
+	def test_tema1_prueba_9(self):
+		result = tema1.etiquetado_consumo_energia(130)
+		self.assertEquals(result, "escala: G , eficiencia: alto consumo energético", "failure")
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()

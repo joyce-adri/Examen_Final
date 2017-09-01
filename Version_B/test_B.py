3110708c6412b5977361f9c43d1d142fece7c4f9
jebenite <evelynbenitez263@gmail.com>
2017-09-01 09:21:13 -0500

    tema1_test7 al 10

--- Version_B/test_B.py
+++ Version_B/test_B.py
@@ -14,27 +14,41 @@ class Test(unittest.TestCase):
 		msg=tema1.premio_por_deposito_a_plazo_fijo(120,60001)
 		self.assertEquals(msg,"Tipo de premio: mini nevera electrolux")
 
-	def test_tema2_prueba_2(self):
+	def test_tema1_prueba_2(self):
 		msg=tema1.premio_por_deposito_a_plazo_fijo(120,30001)
 		self.assertEquals(msg,"Tipo de premio: aspiradora electrolux")
 	
-	def test_tema2_prueba_3(self):
+	def test_tema1_prueba_3(self):
 		msg=tema1.premio_por_deposito_a_plazo_fijo(120,15001)
 		self.assertEquals(msg,"Tipo de premio: arrocera electrolux")
 	
-	def test_tema2_prueba_4(self):
+	def test_tema1_prueba_4(self):
 		msg=tema1.premio_por_deposito_a_plazo_fijo(180,60001)
 		self.assertEquals(msg,"Tipo de premio: minicomponente LG")
 	
-	def test_tema2_prueba_5(self):
+	def test_tema1_prueba_5(self):
 		msg=tema1.premio_por_deposito_a_plazo_fijo(180,30001)
 		self.assertEquals(msg,"Tipo de premio: samsumg galaxy J1")
 	
-	def test_tema2_prueba_6(self):
+	def test_tema1_prueba_6(self):
 		msg=tema1.premio_por_deposito_a_plazo_fijo(180,15001)
 		self.assertEquals(msg,"Tipo de premio: licuadora electrolux")
 	
+	def test_tema1_prueba_7(self):
+		msg=tema1.premio_por_deposito_a_plazo_fijo(360,65001)
+		self.assertEquals(msg,"Tipo de premio: tv led LG")
 	
+	def test_tema1_prueba_8(self):
+		msg=tema1.premio_por_deposito_a_plazo_fijo(360,35001)
+		self.assertEquals(msg,"Tipo de premio: tablet samsumg")
+	
+	def test_tema1_prueba_9(self):
+		msg=tema1.premio_por_deposito_a_plazo_fijo(360,15001)
+		self.assertEquals(msg,"Tipo de premio: microondas electrolux")
+	
+	def test_tema1_prueba_10(self):
+			msg=tema1.premio_por_deposito_a_plazo_fijo(460,15001)
+			self.assertEquals(msg,"Tipo de premio: no recibe premio")	
 		
 if __name__ == '__main__':
     unittest.main()

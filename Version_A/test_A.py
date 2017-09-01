01cdfb50628aa0cac4a242ae8ca33d1773aaef1a
Israel Zurita <israel.5k@hotmail.com>
2017-09-01 09:25:20 -0500

    Erasmo Zurita tema 2

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -10,13 +10,45 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_tema1_prueba_ID(self):
+    #def test_tema1_prueba_ID(self):
         # inserte su codigo de prueba
-        self.assertEquals()
+    #    self.assertEquals()
 
-    def test_tema2_prueba_ID(self):
+    def test_tema2_prueba_1A(self):
         # inserte su codigo de prueba
-        self.assertEquals()
+        msg = tema2.clasificacion_clientes(30000, True)
+        print msg
+        self.assertEquals(msg,"Tipo de cliente: AAA, Envio de boletin: Si")
+
+    def test_tema2_prueba_2B(self):
+        # inserte su codigo de prueba
+        msg = tema2.clasificacion_clientes(20000, True)
+        print msg
+        self.assertEquals(msg,"Tipo de cliente: AA, Envio de boletin: Si")
+
+    def test_tema2_prueba_3C(self):
+        # inserte su codigo de prueba
+        msg = tema2.clasificacion_clientes(15000, True)
+        print msg
+        self.assertEquals(msg,"Tipo de cliente: A, Envio de boletin: Si")
+
+    def test_tema2_prueba_4D(self):
+        # inserte su codigo de prueba
+        msg = tema2.clasificacion_clientes(10000, True)
+        print msg
+        self.assertEquals(msg,"Tipo de cliente: B, Envio de boletin: Si")
+
+    def test_tema2_prueba_5E(self):
+        # inserte su codigo de prueba
+        msg = tema2.clasificacion_clientes(5000, True)
+        print msg
+        self.assertEquals(msg,"Tipo de cliente: C, Envio de boletin: Si")
+
+    def test_tema2_prueba_6F(self):
+        # inserte su codigo de prueba
+        msg = tema2.clasificacion_clientes(30000, False)
+        print msg
+        self.assertEquals(msg,"Tipo de cliente: no es digno de crédito, Envio de boletin: No")
 
 if __name__ == '__main__':
     unittest.main()

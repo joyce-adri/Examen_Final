5de7d416f95eecca236b01bdba20fcc0c6be9338
Fernando Carvajal <efcu93@gmail.com>
2017-09-01 09:03:35 -0500

    tema 1

--- Version_B/test_B.py
+++ Version_B/test_B.py
@@ -11,9 +11,47 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_tema1_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
+    def test_tema1_prueba_1(self):
+    	premio = premio_por_deposito_a_plazo_fijo(plazo=120, montoInversion=50002)
+        self.assertEquals(premio, "mini nevera electrolux")
+
+    def test_tema1_prueba_2(self):
+    	premio = premio_por_deposito_a_plazo_fijo(plazo=120, montoInversion=20002)
+        self.assertEquals(premio, "aspiradora electrolux")
+
+    def test_tema1_prueba_3(self):
+    	premio = premio_por_deposito_a_plazo_fijo(plazo=120, montoInversion=10002)
+        self.assertEquals(premio, "arrocera electrolux")
+
+    def test_tema1_prueba_4(self):
+    	premio = premio_por_deposito_a_plazo_fijo(plazo=180, montoInversion=50002)
+    	self.assertEquals(premio, "minicomponente LG")
+
+    def test_tema1_prueba_5(self):
+    	premio = premio_por_deposito_a_plazo_fijo(plazo=180, montoInversion=20002)
+    	self.assertEquals(premio, "samsumg galaxy J1")
+
+    def test_tema1_prueba_6(self):
+    	premio = premio_por_deposito_a_plazo_fijo(plazo=180, montoInversion=10002)
+    	self.assertEquals(premio, "licuadora electrolux")
+
+    def test_tema1_prueba_7(self):
+    	premio = premio_por_deposito_a_plazo_fijo(plazo=360, montoInversion=50002)
+    	self.assertEquals(premio, "tv led LG")
+
+    def test_tema1_prueba_8(self):
+    	premio = premio_por_deposito_a_plazo_fijo(plazo=360, montoInversion=20002)
+    	self.assertEquals(premio, "tablet samsumg")
+
+    def test_tema1_prueba_9(self):
+    	premio = premio_por_deposito_a_plazo_fijo(plazo=360, montoInversion=10002)
+    	self.assertEquals(premio, "microondas electrolux")
+
+
+
+
+
+
 
     def test_tema2_prueba_ID(self):
         # inserte su codigo de prueba

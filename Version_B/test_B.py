007b1a92eb4c23f8fd8f5fd5f94cb6cf968e8de8
jebenite <evelynbenitez263@gmail.com>
2017-09-01 08:49:53 -0500

    tema1_test1

--- Version_B/test_B.py
+++ Version_B/test_B.py
@@ -10,14 +10,14 @@ class Test(unittest.TestCase):
     
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_tema1_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
+	def test_tema1_prueba_1(self):
+		msg=tema1.premio_por_deposito_a_plazo_fijo(120,60001)
+		self.assertEquals(msg,"Tipo de premio: mini nevera electrolux")
 
-    def test_tema2_prueba_ID(self):
+
+    #def test_tema2_prueba_ID(self):
         # inserte su codigo de prueba
-        self.assertEquals()
+        #self.assertEquals()
 
 if __name__ == '__main__':
     unittest.main()

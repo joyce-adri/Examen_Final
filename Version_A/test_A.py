6e92716e9187a50ceb19c136beeb7b58982a4ac3
Israel Zurita <israel.5k@hotmail.com>
2017-09-01 09:32:06 -0500

    Erasmo Zurita tema 2 correciones

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -18,37 +18,37 @@ class Test(unittest.TestCase):
         # inserte su codigo de prueba
         msg = tema2.clasificacion_clientes(30000, True)
         print msg
-        self.assertEquals(msg,"Tipo de cliente: AAA, Envio de boletin: Si")
+        self.assertAlmostEquals(msg,"Tipo de cliente: AAA, Envio de boletin: Si")
 
     def test_tema2_prueba_2B(self):
         # inserte su codigo de prueba
         msg = tema2.clasificacion_clientes(20000, True)
         print msg
-        self.assertEquals(msg,"Tipo de cliente: AA, Envio de boletin: Si")
+        self.assertAlmostEquals(msg,"Tipo de cliente: AA, Envio de boletin: Si")
 
     def test_tema2_prueba_3C(self):
         # inserte su codigo de prueba
         msg = tema2.clasificacion_clientes(15000, True)
         print msg
-        self.assertEquals(msg,"Tipo de cliente: A, Envio de boletin: Si")
+        self.assertAlmostEquals(msg,"Tipo de cliente: A, Envio de boletin: Si")
 
     def test_tema2_prueba_4D(self):
         # inserte su codigo de prueba
         msg = tema2.clasificacion_clientes(10000, True)
         print msg
-        self.assertEquals(msg,"Tipo de cliente: B, Envio de boletin: Si")
+        self.assertAlmostEquals(msg,"Tipo de cliente: B, Envio de boletin: Si")
 
     def test_tema2_prueba_5E(self):
         # inserte su codigo de prueba
         msg = tema2.clasificacion_clientes(5000, True)
         print msg
-        self.assertEquals(msg,"Tipo de cliente: C, Envio de boletin: Si")
+        self.assertAlmostEquals(msg,"Tipo de cliente: C, Envio de boletin: Si")
 
     def test_tema2_prueba_6F(self):
         # inserte su codigo de prueba
         msg = tema2.clasificacion_clientes(30000, False)
         print msg
-        self.assertEquals(msg,"Tipo de cliente: no es digno de crédito, Envio de boletin: No")
+        self.assertAlmostEquals(msg,"Tipo de cliente: no es digno de crédito, Envio de boletin: No")
 
 if __name__ == '__main__':
     unittest.main()

d90d1996941aa21f1df4fb93c666fa5c7c1f2c16
Juan Alvarado <CltControl@GGIECSOFWRK160.espol.edu.ec>
2017-09-01 08:57:51 -0500

    Add tema1 test 2, 3, 4, 5

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -15,6 +15,33 @@ class Test(unittest.TestCase):
         etiqueta = tema1.etiquetado_consumo_energia(0)
         esperado = "escala: %s , eficiencia: %s" % ("A++", "los más eficientes")
         self.assertEquals(etiqueta, esperado)
+        print(etiqueta+"\n")
+
+    def test_tema1_prueba_2(self):
+        # inserte su codigo de prueba
+        etiqueta = tema1.etiquetado_consumo_energia(30)
+        esperado = "escala: %s , eficiencia: %s" % ("A+", "los más eficientes")
+        self.assertEquals(etiqueta, esperado)
+        print(etiqueta + "\n")
+
+    def test_tema1_prueba_3(self):
+        # inserte su codigo de prueba
+        etiqueta = tema1.etiquetado_consumo_energia(42)
+        esperado = "escala: %s , eficiencia: %s" % ("A", "los más eficientes")
+        self.assertEquals(etiqueta, esperado)
+        print(etiqueta + "\n")
+
+    def test_tema1_prueba_4(self):
+        # inserte su codigo de prueba
+        etiqueta = tema1.etiquetado_consumo_energia(55)
+        esperado = "escala: %s , eficiencia: %s" % ("B", "los más eficientes")
+        self.assertEquals(etiqueta, esperado)
+
+    def test_tema1_prueba_5(self):
+        # inserte su codigo de prueba
+        etiqueta = tema1.etiquetado_consumo_energia(75)
+        esperado = "escala: %s , eficiencia: %s" % ("C", "los más eficientes")
+        self.assertEquals(etiqueta, esperado)
 
     def test_tema2_prueba_ID(self):
         # inserte su codigo de prueba

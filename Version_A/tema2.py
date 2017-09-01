aa6420f92218783d45790d28387660c72562352a
odmoreno <oscarpol95@gmail.com>
2017-09-01 09:13:42 -0500

    Test del tema 2

--- Version_A/tema2.py
+++ Version_A/tema2.py
@@ -4,16 +4,16 @@ from __future__ import unicode_literals
 def clasificacion_clientes(montoDepositado, informacionBuro):
     enviarBoletinInformativo = "No"
     clasificacionCliente = "no es digno de crédito"
-    if informacionBuro:  # True: digno de credito, False: no es digno de credito
-        if montoDepositado >= 30000:
-            clasificacionCliente = "AAA"
-        elif montoDepositado >= 20000 and montoDepositado < 30000:
-            clasificacionCliente = "AA"
-        elif montoDepositado >= 15000 and montoDepositado < 20000:
-            clasificacionCliente = "A"
-        elif montoDepositado >= 10000 and montoDepositado < 15000:
-            clasificacionCliente = "B"
-        elif montoDepositado >= 5000 and montoDepositado < 10000:
-            clasificacionCliente = "C"
-        enviarBoletinInformativo = "Si"
-    return "Tipo de cliente: %s, Envio de boletin: %s" % (clasificacionCliente, enviarBoletinInformativo)
+    if informacionBuro:  # True: digno de credito, False: no es digno de credito  # Nodo 1
+        if montoDepositado >= 30000:  #Nodo 2
+            clasificacionCliente = "AAA" #Nodo 3
+        elif montoDepositado >= 20000 and montoDepositado < 30000: #Nodo 4
+            clasificacionCliente = "AA" #Nodo 5
+        elif montoDepositado >= 15000 and montoDepositado < 20000: #Nodo 6
+            clasificacionCliente = "A" #Nodo 7
+        elif montoDepositado >= 10000 and montoDepositado < 15000: #Nodo 8
+            clasificacionCliente = "B" #Nodo 9
+        elif montoDepositado >= 5000 and montoDepositado < 10000: #Nodo 10
+            clasificacionCliente = "C" #Nodo 11
+        enviarBoletinInformativo = "Si" #Nodo 12
+    return "Tipo de cliente: %s, Envio de boletin: %s" % (clasificacionCliente, enviarBoletinInformativo) #Nodo 13
--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -2,22 +2,37 @@
 from __future__ import unicode_literals
 
 import unittest
-import tema1
-import tema2
 
-"prueba"
+from Version_A.tema2 import clasificacion_clientes
+
 class Test(unittest.TestCase):
     
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_tema1_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
 
-    def test_tema2_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
+    def test_tema2_1(self):
+        msg = clasificacion_clientes(3000, False)
+        self.assertEqual(msg, "Tipo de cliente: no es digno de crédito, Envio de boletin: No")
+
+    def test_tema2_2(self):
+        msg = clasificacion_clientes(30000, True)
+        self.assertEqual(msg, "Tipo de cliente: AAA, Envio de boletin: Si")
+
+    def test_tema2_3(self):
+        msg = clasificacion_clientes(20001, True)
+        self.assertEqual(msg, "Tipo de cliente: AA, Envio de boletin: Si")
+
+    def test_tema2_4(self):
+        msg = clasificacion_clientes(15001, True)
+        self.assertEqual(msg, "Tipo de cliente: A, Envio de boletin: Si")
+
+    def test_tema2_5(self):
+        msg = clasificacion_clientes(10001, True)
+        self.assertEqual(msg, "Tipo de cliente: B, Envio de boletin: Si")
+
+    def test_tema2_6(self):
+        msg = clasificacion_clientes(5001, True)
+        self.assertEqual(msg, "Tipo de cliente: C, Envio de boletin: Si")
 
 if __name__ == '__main__':
     unittest.main()

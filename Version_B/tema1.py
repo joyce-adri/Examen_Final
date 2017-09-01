7bca0e51ed8bc1d29d121cd396cce27f84b529e4
julio93 <juliohbk_93@hotmail.es>
2017-09-01 09:30:20 -0500

    Update tema1.py
    añadiendo la funcion deposito fijo

--- Version_B/tema1.py
+++ Version_B/tema1.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-def premio_por_deposito_a_plazo_fijo(plazo, montoInversion):
-    premio = "no recibe premio"
-    if plazo is 120:
-        if montoInversion >= 50001:
-            premio = "mini nevera electrolux"
-        elif montoInversion >= 20001 and montoInversion < 50001:
-            premio = "aspiradora electrolux"
-        elif montoInversion >= 10001 and montoInversion < 20001:
-            premio = "arrocera electrolux"
-    elif plazo is 180:
-        if montoInversion >= 50001:
-            premio = "minicomponente LG"
-        elif montoInversion >= 20001 and montoInversion < 50001:
-            premio = "samsumg galaxy J1"
-        elif montoInversion >= 10001 and montoInversion < 20001:
-            premio = "licuadora electrolux"
-    elif plazo is 360:
-        if montoInversion >= 50001:
-            premio = "tv led LG"
-        elif montoInversion >= 20001 and montoInversion < 50001:
-            premio = "tablet samsumg"
-        elif montoInversion >= 10001 and montoInversion < 20001:
-            premio = "microondas electrolux"
-    return "Tipo de premio: %s" % premio
+class Banco:
+
+    def premios_depositos_plazo_fijo(dias, monto):
+        if (dias == 120):
+            if(monto>=10.001 and monto <=20000):
+                return "Arrocera Electrolux 1.8L"
+            elif(monto>=20.001 and monto <=50000):
+                return "Aspiradora Electrolux"
+            elif (monto > 50.001):
+                return "Mini nevera 5 pies Electrolux"
+
+        elif (dias==180):
+            if (monto >= 10.001 and monto <= 20000):
+                return "Licuadora 5 velocidades Electrolux"
+            elif (monto >= 20.001 and monto <= 50000):
+                return "Samsung Galaxy J1"
+            elif (monto > 50.001):
+                return "Minicomponente LG"
+
+        elif (dias==360):
+            if (monto >= 10.001 and monto <= 20000):
+                return "Microondas Electrolux 20L"
+            elif (monto >= 20.001 and monto <= 50000):
+                return "Tablet Samsung"
+            elif (monto > 50.001):
+                return "Tv LED 32 LG"

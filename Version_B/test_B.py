be18bd8f2a84208158495cf5d773248c28f2b80c
jebenite <evelynbenitez263@gmail.com>
2017-09-01 10:08:34 -0500

    tema2_todos los testd

--- Version_B/test_B.py
+++ Version_B/test_B.py
@@ -48,7 +48,30 @@ class Test(unittest.TestCase):
 	
 	def test_tema1_prueba_10(self):
 			msg=tema1.premio_por_deposito_a_plazo_fijo(460,15001)
-			self.assertEquals(msg,"Tipo de premio: no recibe premio")	
-		
+			self.assertEquals(msg,"Tipo de premio: no recibe premio")
+	
+	def test_tema2_prueba_1(self):
+			msg=tema2.ofrecer_tarjeta_con_chip(20001,"AAA")
+			self.assertEquals(msg,"Tipo de tarjeta: Gold")
+			
+	def test_tema2_prueba_2(self):
+			msg=tema2.ofrecer_tarjeta_con_chip(16000,"AA")
+			self.assertEquals(msg,"Tipo de tarjeta: Platinum")	
+			
+	def test_tema2_prueba_3(self):
+			msg=tema2.ofrecer_tarjeta_con_chip(12000,"A")
+			self.assertEquals(msg,"Tipo de tarjeta: Internacional")	
+	
+	def test_tema2_prueba_4(self):
+			msg=tema2.ofrecer_tarjeta_con_chip(6000,"B")
+			self.assertEquals(msg,"Tipo de tarjeta: Club Miles")
+	
+	def test_tema2_prueba_5(self):
+			msg=tema2.ofrecer_tarjeta_con_chip(4000,"C")
+			self.assertEquals(msg,"Tipo de tarjeta: Advantage")
+	
+	def test_tema2_prueba_6(self):
+			msg=tema2.ofrecer_tarjeta_con_chip(2000,"D")
+			self.assertEquals(msg,"Tipo de tarjeta: Clasica")
 if __name__ == '__main__':
     unittest.main()

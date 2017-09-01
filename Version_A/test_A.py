e5a247241f7d99ebc08ef2c6d393f050c23fe07f
Julian Adams <jadams@espol.edu.ec>
2017-09-01 09:06:37 -0500

    Tema 1, test 1 (resubido por Travis config)

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -14,32 +14,7 @@ class Test(unittest.TestCase):
     def test_tema1_prueba_1(self):
         eti = etiquetado_consumo_energia(20)
         self.assertEquals(eti, "escala: A++ , eficiencia: los más eficientes")
-    def test_tema1_prueba_2(self):
-        eti = etiquetado_consumo_energia(32)
-        self.assertEquals(eti, "escala: A+ , eficiencia: los más eficientes")
-    def test_tema1_prueba_3(self):
-        eti = etiquetado_consumo_energia(45)
-        self.assertEquals(eti, "escala: A , eficiencia: los más eficientes")
-    def test_tema1_prueba_4(self):
-        eti = etiquetado_consumo_energia(60)
-        self.assertEquals(eti, "escala: B , eficiencia: los más eficientes")
-    def test_tema1_prueba_5(self):
-        eti = etiquetado_consumo_energia(80)
-        self.assertEquals(eti, "escala: C , eficiencia: los más eficientes")
 
-    def test_tema1_prueba_6(self):
-        eti = etiquetado_consumo_energia(95)
-        self.assertEquals(eti, "escala: D , eficiencia: consumo medio")
-    def test_tema1_prueba_7(self):
-        eti = etiquetado_consumo_energia(101)
-        self.assertEquals(eti, "escala: E , eficiencia: consumo medio")
-
-    def test_tema1_prueba_8(self):
-        eti = etiquetado_consumo_energia(115)
-        self.assertEquals(eti, "escala: F , eficiencia: alto consumo energético")
-    def test_tema1_prueba_9(self):
-        eti = etiquetado_consumo_energia(130)
-        self.assertEquals(eti, "escala: G , eficiencia: alto consumo energético")
 	#============================================#
 """
     def test_tema2_prueba_ID(self):

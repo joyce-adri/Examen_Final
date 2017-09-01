94c50534f1ff1fc136da847a1bb7b8a4953ed124
Julian Adams <jadams@espol.edu.ec>
2017-09-01 09:51:31 -0500

    Tema 2, tests del 1 al 6

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -3,10 +3,10 @@ from __future__ import unicode_literals
 
 import unittest
 import tema1
-#import tema2
+import tema2
 
 from tema1 import etiquetado_consumo_energia
-#from tema2 import clasificacion_clientes
+from tema2 import clasificacion_clientes
 
 class Test(unittest.TestCase):
     
@@ -47,11 +47,25 @@ class Test(unittest.TestCase):
         eti = etiquetado_consumo_energia(-10)
         self.assertEquals(eti, "escala: ninguna , eficiencia: ninguno")
     #============================================#
-"""
+
     def test_tema2_prueba_1(self):
         msj = clasificacion_clientes(5000, False)
         self.assertEquals(msj, "Tipo de cliente: no es digno de crédito, Envio de boletin: No")
-"""
+    def test_tema2_prueba_2(self):
+        msj = clasificacion_clientes(40000, True)
+        self.assertEquals(msj, "Tipo de cliente: AAA, Envio de boletin: Si")
+    def test_tema2_prueba_3(self):
+        msj = clasificacion_clientes(25000, True)
+        self.assertEquals(msj, "Tipo de cliente: AA, Envio de boletin: Si")
+    def test_tema2_prueba_4(self):
+        msj = clasificacion_clientes(17000, True)
+        self.assertEquals(msj, "Tipo de cliente: A, Envio de boletin: Si")
+    def test_tema2_prueba_5(self):
+        msj = clasificacion_clientes(12000, True)
+        self.assertEquals(msj, "Tipo de cliente: B, Envio de boletin: Si")
+    def test_tema2_prueba_6(self):
+        msj = clasificacion_clientes(6000, True)
+        self.assertEquals(msj, "Tipo de cliente: C, Envio de boletin: Si")
 
 if __name__ == '__main__':
     unittest.main()

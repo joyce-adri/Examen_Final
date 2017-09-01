7344842f702eda7628121395c8b826dc83b0246b
Ivan Mera <ivan@cajanegra.com.ec>
2017-09-01 10:05:04 -0500

    Tema 2 Prueba 5 (corr)

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -62,9 +62,9 @@ class Test(unittest.TestCase):
 		result = tema2.clasificacion_clientes(17000, True)
 		self.assertEquals(result, "Tipo de cliente: A, Envio de boletin: Si", "failure")
 
-	# def test_tema2_prueba_5(self):
-	# 	result = tema2.clasificacion_clientes(12000, True)
-	# 	self.assertEquals(result, "Tipo de cliente: B, Envio de boletin: Si", "failure")
+	def test_tema2_prueba_5(self):
+		result = tema2.clasificacion_clientes(12000, True)
+		self.assertEquals(result, "Tipo de cliente: B, Envio de boletin: Si", "failure")
 
 	# def test_tema2_prueba_6(self):
 	# 	result = tema2.clasificacion_clientes(7000, True)

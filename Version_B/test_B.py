07a9fe60985db03b6790d79fd592e38834a54d23
mavemore <veronica940413@hotmail.com>
2017-09-01 09:22:19 -0500

    pruebas Tema 2

--- Version_B/test_B.py
+++ Version_B/test_B.py
@@ -48,23 +48,114 @@ class Test(unittest.TestCase):
 		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
 		self.assertEqual(premio,"Tipo de premio: licuadora electrolux")
 
-	def test_tema1_prueba_7(self):
-		plazo = 360
-		montoInversion = 50005
-		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
-		self.assertEqual(premio,"Tipo de premio: tv led LG")
+	# def test_tema1_prueba_7(self):
+	# 	plazo = 360
+	# 	montoInversion = 50005
+	# 	premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+	# 	self.assertEqual(premio,"Tipo de premio: tv led LG")
 
-	def test_tema1_prueba_8(self):
-		plazo = 360
-		montoInversion = 20005
-		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
-		self.assertEqual(premio,"Tipo de premio: tablet samsumg")
+	# def test_tema1_prueba_8(self):
+	# 	plazo = 360
+	# 	montoInversion = 20005
+	# 	premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+	# 	self.assertEqual(premio,"Tipo de premio: tablet samsumg")
 
-	def test_tema1_prueba_9(self):
-		plazo = 360
-		montoInversion = 10005
-		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
-		self.assertEqual(premio,"Tipo de premio: microondas electrolux")
+	# def test_tema1_prueba_9(self):
+	# 	plazo = 360
+	# 	montoInversion = 10005
+	# 	premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+	# 	self.assertEqual(premio,"Tipo de premio: microondas electrolux")
+
+#CASOS PRUEBA TEMA 2
+	def test_tema2_prueba_1(self):
+		tipoCliente = 'AAA'
+		montoDeuda = 20000
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Gold")
+
+	def test_tema2_prueba_2(self):
+		tipoCliente = 'AAA'
+		montoDeuda = 19999
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Clasica")
+
+	def test_tema2_prueba_3(self):
+		tipoCliente = 'AA'
+		montoDeuda = 14999
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Clasica")
+
+	def test_tema2_prueba_4(self):
+		tipoCliente = 'AA'
+		montoDeuda = 20000
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Clasica")
+
+	def test_tema2_prueba_5(self):
+		tipoCliente = 'AA'
+		montoDeuda = 15000
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Platinum")
+
+	def test_tema2_prueba_6(self):
+		tipoCliente = 'A'
+		montoDeuda = 9999
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Clasica")
+
+	def test_tema2_prueba_7(self):
+		tipoCliente = 'A'
+		montoDeuda = 15000
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Clasica")
+
+	def test_tema2_prueba_8(self):
+		tipoCliente = 'A'
+		montoDeuda = 10000
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Internacional")
+
+	def test_tema2_prueba_9(self):
+		tipoCliente = 'B'
+		montoDeuda = 4999
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Clasica")
+
+	def test_tema2_prueba_10(self):
+		tipoCliente = 'B'
+		montoDeuda = 10000
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Clasica")
+
+	def test_tema2_prueba_11(self):
+		tipoCliente = 'B'
+		montoDeuda = 5000
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Club Miles")
+
+	def test_tema2_prueba_12(self):
+		tipoCliente = 'C'
+		montoDeuda = 2999
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Clasica")
+
+	def test_tema2_prueba_13(self):
+		tipoCliente = 'C'
+		montoDeuda = 5000
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Clasica")
+
+	def test_tema2_prueba_14(self):
+		tipoCliente = 'C'
+		montoDeuda = 3000
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Advantage")
+
+	def test_tema2_prueba_15(self):
+		tipoCliente = 'F'
+		montoDeuda = 1000
+		tarjeta = tema2.ofrecer_tarjeta_con_chip(montoDeuda, tipoCliente)
+		self.assertEqual(tarjeta,"Tipo de tarjeta: Clasica")
 
 if __name__ == '__main__':
 	unittest.main()

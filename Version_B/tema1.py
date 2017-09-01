c13b0585b0f5b7ccd886db1fd10db570010ada94
Christian Jaramillo <chaljara@espol.edu.ec>
2017-09-01 09:02:17 -0500

    b1

--- Version_B/tema1.py
+++ Version_B/tema1.py
@@ -1,2 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+
+class PremiosDepositosPlazoFijo:
+    def obtenerPremio(self, plazo_dias, inversion):
+        if inversion >= 10001 and inversion <= 20000:
+            if plazo_dias == 120:
+                return "arrocera electrolux 1.8l"
+            elif plazo_dias == 180:
+                return "licuadora 5 velocidades electrolux"
+            elif plazo_dias == 360:
+                return "microondas electrolux 20l"
+            else :
+                return "error"
+        elif inversion >= 20001 and inversion <= 50000:
+            if plazo_dias == 120:
+                return "aspiradora electrolux"
+            elif plazo_dias == 180:
+                return "samsung galaxy j1"
+            elif plazo_dias == 360:
+                return "tablet samsung"
+            else :
+                return "error"
+        elif inversion >= 50001:
+            if plazo_dias == 120:
+                return "mini nevera 5 pies electrolux"
+            elif plazo_dias == 180:
+                return "minicomponente lg"
+            elif plazo_dias == 360:
+                return "tv led 32 lg"
+            else :
+                return "error"
+        else:
+            return "error"
--- Version_B/test_B.py
+++ Version_B/test_B.py
@@ -5,19 +5,51 @@ import unittest
 import tema1
 import tema2
 
-
 class Test(unittest.TestCase):
     
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_tema1_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
 
-    def test_tema2_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
+    def test_tema1_prueba_1(self):
+        premio = PremiosDepositosPlazoFijo().obtenerPremio(120, 15000)
+        self.assertEquals(premio, "arrocera electrolux 1.8l")
+
+    def test_tema1_prueba_2(self):
+        premio = PremiosDepositosPlazoFijo().obtenerPremio(120 , 25000)
+        self.assertEquals(premio, "aspiradora electrolux")
+
+    def test_tema1_prueba_3(self):
+        premio = PremiosDepositosPlazoFijo().obtenerPremio(120, 100000)
+        self.assertEquals(premio, "mini nevera 5 pies electrolux")
+
+    def test_tema1_prueba_4(self):
+        premio = PremiosDepositosPlazoFijo().obtenerPremio(180, 15000)
+        self.assertEquals(premio, "licuadora 5 velocidades electrolux")
+
+    def test_tema1_prueba_5(self):
+        premio = PremiosDepositosPlazoFijo().obtenerPremio(180, 25000)
+        self.assertEquals(premio, "samsung galaxy j1")
+
+    def test_tema1_prueba_6(self):
+        premio = PremiosDepositosPlazoFijo().obtenerPremio(180, 100000)
+        self.assertEquals(premio, "minicomponente lg")
+
+    def test_tema1_prueba_7(self):
+        premio = PremiosDepositosPlazoFijo().obtenerPremio(360, 15000)
+        self.assertEquals(premio, "microondas electrolux 20l")
+
+    def test_tema1_prueba_8(self):
+        premio = PremiosDepositosPlazoFijo().obtenerPremio(360, 25000)
+        self.assertEquals(premio, "tablet samsung")
+
+    def test_tema1_prueba_9(self):
+        premio = PremiosDepositosPlazoFijo().obtenerPremio(360, 100000)
+        self.assertEquals(premio, "tv led 32 lg")
+
+
+    #def test_tema2_prueba_ID(self):
+     #   # inserte su codigo de prueba
+     #   self.assertEquals()
 
 if __name__ == '__main__':
     unittest.main()

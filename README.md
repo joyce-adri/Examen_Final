b66acd722b0d722389b10a72f7f5c40b3f5398d7
joyce-adri <joyce01sm@gmail.com>
2017-09-01 08:04:20 -0500

    archivos con los codigos fuente

--- README.md
+++ README.md
@@ -3,5 +3,23 @@
 # - Caja blanca: grafos y plan de pruebas
 # - Utilizar Travis y Coveralls para evidenciar la ejecución y cobertura de los casos de prueba
 
+Instrucciones 
+
+1) Crear una rama con su nombre completo ejemplo:
+>> git checkout -b joyce_adriana_sarmiento
+
+2) De acuerdo a la versión del examen A o B ingresar a los folder denominados:
+Version_A o Version_B
+
+3) revisar los archivos con el codigo fuente de cada tema respectivamente:
+tema1.py y tema2.py
+
+4) en el mismo directorio ingresar al archivo para CODIFCAR los casos de prueba:
+test_A.py
+test_B.py
+
+5) ejecutar las pruebas localmente y si son exitosas subirlas en su rama
+
+
 
 [![Coverage Status](https://coveralls.io/repos/github/joyce-adri/Examen_Final/badge.svg?branch=master)](https://coveralls.io/github/joyce-adri/Examen_Final?branch=master)
--- Version_A/tema1.py
+++ Version_A/tema1.py
@@ -1,2 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+def etiquetado_consumo_energia(porcentaje):
+    clasificacion_escala_int = "ninguna"
+    rango = "ninguno"
+    if porcentaje >= 0 and porcentaje < 90:
+        rango = "los más eficientes"
+        if porcentaje < 30 and porcentaje >= 0 :
+            clasificacion_escala_int = "A++"
+        elif porcentaje >= 30 and porcentaje < 42 :
+            clasificacion_escala_int = "A+"
+        elif porcentaje >= 42 and porcentaje < 55 :
+            clasificacion_escala_int = "A"
+        elif porcentaje >= 55 and porcentaje < 75 :
+            clasificacion_escala_int = "B"
+        elif porcentaje >= 75 and porcentaje < 90 :
+            clasificacion_escala_int = "C"
+    elif porcentaje >= 90 and porcentaje < 110:
+        rango = "consumo medio"
+        if porcentaje >= 90 and porcentaje < 100 :
+            clasificacion_escala_int = "D"
+        elif porcentaje >= 100 and porcentaje < 110 :
+            clasificacion_escala_int = "E"
+    elif porcentaje >= 110:
+        rango = "alto consumo energético"
+        if porcentaje >= 110 and porcentaje <= 125 :
+            clasificacion_escala_int = "F"
+        elif porcentaje > 125 :
+            clasificacion_escala_int = "G"
+    etiqueta = "escala: %s , eficiencia: %s" % (clasificacion_escala_int, rango)
+    return etiqueta

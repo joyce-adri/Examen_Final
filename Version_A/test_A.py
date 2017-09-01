85c3f0cbee6e90165652763627b86254dd71aa7f
Ivan Mera <ivan@cajanegra.com.ec>
2017-09-01 10:10:15 -0500

    Tema 1 Prueba 2 (corr)

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -14,9 +14,9 @@ class Test(unittest.TestCase):
 		result = tema1.etiquetado_consumo_energia(20)
 		self.assertEquals(result, "escala: A++ , eficiencia: los más eficientes", "failure")
 
-	# def test_tema1_prueba_2(self):
-	# 	result = tema1.etiquetado_consumo_energia(40)
-	# 	self.assertEquals(result, "escala: A+ , eficiencia: los más eficientes", "failure")
+	def test_tema1_prueba_2(self):
+		result = tema1.etiquetado_consumo_energia(40)
+		self.assertEquals(result, "escala: A+ , eficiencia: los más eficientes", "failure")
 
 	# def test_tema1_prueba_3(self):
 	# 	result = tema1.etiquetado_consumo_energia(50)

b5301cd6261cd37164076929b32e2bb8dcb04953
Erick Pérez <erialper@espol.edu.ec>
2017-09-01 09:01:57 -0500

    prueba 2

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -12,10 +12,45 @@ class Test(unittest.TestCase):
     
     def test_tema1_prueba_1(self):
         # inserte su codigo de prueba
+        print("Prueba 1")
         msg = tema1.etiquetado_consumo_energia(60)
-        print(msg)
         self.assertEquals(msg, "escala: B , eficiencia: los más eficientes")
 
+    def test_tema1_prueba_2(self):
+        # inserte su codigo de prueba
+        print("Prueba 1")
+        msg = tema1.etiquetado_consumo_energia(60)
+        self.assertEquals(msg, "escala: B , eficiencia: los más eficientes")
+
+    def test_tema1_prueba_3(self):
+        # inserte su codigo de prueba
+        print("Prueba 1")
+        msg = tema1.etiquetado_consumo_energia(60)
+        self.assertEquals(msg, "escala: B , eficiencia: los más eficientes")
+
+    def test_tema1_prueba_4(self):
+        # inserte su codigo de prueba
+        print("Prueba 1")
+        msg = tema1.etiquetado_consumo_energia(60)
+        self.assertEquals(msg, "escala: B , eficiencia: los más eficientes")
+
+    def test_tema1_prueba_5(self):
+        # inserte su codigo de prueba
+        print("Prueba 1")
+        msg = tema1.etiquetado_consumo_energia(60)
+        self.assertEquals(msg, "escala: B , eficiencia: los más eficientes")
+
+    def test_tema1_prueba_6(self):
+        # inserte su codigo de prueba
+        print("Prueba 1")
+        msg = tema1.etiquetado_consumo_energia(60)
+        self.assertEquals(msg, "escala: B , eficiencia: los más eficientes")
+
+    def test_tema1_prueba_7(self):
+        # inserte su codigo de prueba
+        print("Prueba 1")
+        msg = tema1.etiquetado_consumo_energia(60)
+        self.assertEquals(msg, "escala: B , eficiencia: los más eficientes")
 
     #def test_tema2_prueba_ID(self):
         # inserte su codigo de prueba

061972189aefe8e23102b12187b10bcf409c93ba
mavemore <veronica940413@hotmail.com>
2017-09-01 08:40:59 -0500

    pruebas Tema 1

--- Version_B/test_B.py
+++ Version_B/test_B.py
@@ -7,17 +7,64 @@ import tema2
 
 
 class Test(unittest.TestCase):
-    
+	
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
-    
-    def test_tema1_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
 
-    def test_tema2_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
+#CASOS PRUEBA TEMA 1
+	def test_tema1_prueba_1(self):
+		plazo = 120
+		montoInversion = 50005
+		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+		self.assertEqual(premio,"Tipo de premio: mini nevera electrolux")
+
+	def test_tema1_prueba_2(self):
+		plazo = 120
+		montoInversion = 20005
+		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+		self.assertEqual(premio,"Tipo de premio: aspiradora electrolux")
+
+	def test_tema1_prueba_3(self):
+		plazo = 120
+		montoInversion = 10005
+		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+		self.assertEqual(premio,"Tipo de premio: arrocera electrolux")
+
+	def test_tema1_prueba_4(self):
+		plazo = 180
+		montoInversion = 50005
+		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+		self.assertEqual(premio,"Tipo de premio: minicomponente LG")
+
+	def test_tema1_prueba_5(self):
+		plazo = 180
+		montoInversion = 20005
+		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+		self.assertEqual(premio,"Tipo de premio: samsumg galaxy J1")
+
+	def test_tema1_prueba_6(self):
+		plazo = 180
+		montoInversion = 10005
+		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+		self.assertEqual(premio,"Tipo de premio: licuadora electrolux")
+
+	def test_tema1_prueba_7(self):
+		plazo = 360
+		montoInversion = 50005
+		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+		self.assertEqual(premio,"Tipo de premio: tv led LG")
+
+	def test_tema1_prueba_8(self):
+		plazo = 360
+		montoInversion = 20005
+		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+		self.assertEqual(premio,"Tipo de premio: tablet samsumg")
+
+	def test_tema1_prueba_9(self):
+		plazo = 360
+		montoInversion = 10005
+		premio = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+		self.assertEqual(premio,"Tipo de premio: microondas electrolux")
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()

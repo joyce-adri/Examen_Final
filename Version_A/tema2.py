17044e6b8def51c4aca3dec3271390a945d9c97d
Erick Pérez <erialper@espol.edu.ec>
2017-09-01 10:00:40 -0500

    Tema 2: Completo

--- Version_A/tema2.py
+++ Version_A/tema2.py
@@ -2,18 +2,18 @@
 from __future__ import unicode_literals
 
 def clasificacion_clientes(montoDepositado, informacionBuro):
-    enviarBoletinInformativo = "No"
-    clasificacionCliente = "no es digno de crédito"
-    if informacionBuro:  # True: digno de credito, False: no es digno de credito
-        if montoDepositado >= 30000:
+    enviarBoletinInformativo = "No"    #1
+    clasificacionCliente = "no es digno de crédito"   #1
+    if informacionBuro:  # True: digno de credito, False: no es digno de credito   #2
+        if montoDepositado >= 30000:   #3
             clasificacionCliente = "AAA"
-        elif montoDepositado >= 20000 and montoDepositado < 30000:
+        elif montoDepositado >= 20000 and montoDepositado < 30000:    #4  #5
             clasificacionCliente = "AA"
-        elif montoDepositado >= 15000 and montoDepositado < 20000:
+        elif montoDepositado >= 15000 and montoDepositado < 20000:    #6  #7
             clasificacionCliente = "A"
-        elif montoDepositado >= 10000 and montoDepositado < 15000:
+        elif montoDepositado >= 10000 and montoDepositado < 15000:   #8   #9
             clasificacionCliente = "B"
-        elif montoDepositado >= 5000 and montoDepositado < 10000:
+        elif montoDepositado >= 5000 and montoDepositado < 10000:    #10  #11
             clasificacionCliente = "C"
-        enviarBoletinInformativo = "Si"
-    return "Tipo de cliente: %s, Envio de boletin: %s" % (clasificacionCliente, enviarBoletinInformativo)
+        enviarBoletinInformativo = "Si"   #F
+    return "Tipo de cliente: %s, Envio de boletin: %s" % (clasificacionCliente, enviarBoletinInformativo)   #F
--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -12,67 +12,101 @@ class Test(unittest.TestCase):
     
     def test_tema1_prueba_1(self):
         # inserte su codigo de prueba
-        print("Prueba 1")
+        print("T1 Prueba 1")
         msg = tema1.etiquetado_consumo_energia(-1)
         self.assertEquals(msg, "escala: ninguna , eficiencia: ninguno")
 
     def test_tema1_prueba_2(self):
         # inserte su codigo de prueba
-        print("Prueba 2")
+        print("T1 Prueba 2")
         msg = tema1.etiquetado_consumo_energia(25)
         self.assertEquals(msg, "escala: A++ , eficiencia: los más eficientes")
 
     def test_tema1_prueba_3(self):
         # inserte su codigo de prueba
-        print("Prueba 3")
+        print("T1 Prueba 3")
         msg = tema1.etiquetado_consumo_energia(35)
         self.assertEquals(msg, "escala: A+ , eficiencia: los más eficientes")
 
     def test_tema1_prueba_4(self):
         # inserte su codigo de prueba
-        print("Prueba 4")
+        print("T1 Prueba 4")
         msg = tema1.etiquetado_consumo_energia(45)
         self.assertEquals(msg, "escala: A , eficiencia: los más eficientes")
 
     def test_tema1_prueba_5(self):
         # inserte su codigo de prueba
-        print("Prueba 5")
+        print("T1 Prueba 5")
         msg = tema1.etiquetado_consumo_energia(55)
         self.assertEquals(msg, "escala: B , eficiencia: los más eficientes")
 
     def test_tema1_prueba_6(self):
         # inserte su codigo de prueba
-        print("Prueba 6")
+        print("T1 Prueba 6")
         msg = tema1.etiquetado_consumo_energia(75)
         self.assertEquals(msg, "escala: C , eficiencia: los más eficientes")
 
     def test_tema1_prueba_7(self):
         # inserte su codigo de prueba
-        print("Prueba 7")
+        print("T1 Prueba 7")
         msg = tema1.etiquetado_consumo_energia(95)
         self.assertEquals(msg, "escala: D , eficiencia: consumo medio")
 
     def test_tema1_prueba_8(self):
         # inserte su codigo de prueba
-        print("Prueba 8")
+        print("T1 Prueba 8")
         msg = tema1.etiquetado_consumo_energia(105)
         self.assertEquals(msg, "escala: E , eficiencia: consumo medio")
 
     def test_tema1_prueba_9(self):
         # inserte su codigo de prueba
-        print("Prueba 9")
+        print("T1 Prueba 9")
         msg = tema1.etiquetado_consumo_energia(115)
         self.assertEquals(msg, "escala: F , eficiencia: alto consumo energético")
 
     def test_tema1_prueba_10(self):
         # inserte su codigo de prueba
-        print("Prueba 10")
+        print("T1 Prueba 10")
         msg = tema1.etiquetado_consumo_energia(135)
         self.assertEquals(msg, "escala: G , eficiencia: alto consumo energético")
 
-    #def test_tema2_prueba_ID(self):
+#-----------------------------------------------------------------------------------------------------------------------
+
+    def test_tema2_prueba_1(self):
         # inserte su codigo de prueba
-    #    self.assertEquals()
+        print("T2 Prueba 1")
+        msg = tema2.clasificacion_clientes(1000,False)
+        self.assertEquals(msg, "Tipo de cliente: no es digno de crédito, Envio de boletin: No")
+
+    def test_tema2_prueba_2(self):
+        # inserte su codigo de prueba
+        print("T2 Prueba 2")
+        msg = tema2.clasificacion_clientes(40000,True)
+        self.assertEquals(msg, "Tipo de cliente: AAA, Envio de boletin: Si")
+
+    def test_tema2_prueba_3(self):
+        # inserte su codigo de prueba
+        print("T2 Prueba 3")
+        msg = tema2.clasificacion_clientes(25000, True)
+        self.assertEquals(msg, "Tipo de cliente: AA, Envio de boletin: Si")
+
+    def test_tema2_prueba_4(self):
+        # inserte su codigo de prueba
+        print("T2 Prueba 4")
+        msg = tema2.clasificacion_clientes(18000, True)
+        self.assertEquals(msg, "Tipo de cliente: A, Envio de boletin: Si")
+
+    def test_tema2_prueba_5(self):
+        # inserte su codigo de prueba
+        print("T2 Prueba 5")
+        msg = tema2.clasificacion_clientes(12000,True)
+        self.assertEquals(msg, "Tipo de cliente: B, Envio de boletin: Si")
+
+    def test_tema2_prueba_6(self):
+        # inserte su codigo de prueba
+        print("T2 Prueba 6")
+        msg = tema2.clasificacion_clientes(6000,True)
+        self.assertEquals(msg, "Tipo de cliente: C, Envio de boletin: Si")
 
 if __name__ == '__main__':
     unittest.main()

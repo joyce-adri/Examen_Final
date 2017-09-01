badc1630cd2576a7a160761afed30ca6e18b4006
Julian Adams <jadams@espol.edu.ec>
2017-09-01 08:50:31 -0500

    Tema 1, test 4

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -20,6 +20,9 @@ class Test(unittest.TestCase):
     def test_tema1_prueba_3(self):
         eti = etiquetado_consumo_energia(45)
         self.assertEquals(eti, "escala: A , eficiencia: los más eficientes")
+    def test_tema1_prueba_4(self):
+        eti = etiquetado_consumo_energia(60)
+        self.assertEquals(eti, "escala: B , eficiencia: los más eficientes")
 	#============================================#
 """
     def test_tema2_prueba_ID(self):

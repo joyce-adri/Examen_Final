2ff754c72de19df3d4e27c5abe984809d2591f89
julio93 <juliohbk_93@hotmail.es>
2017-09-01 10:12:42 -0500

    Cambio Final Test_B.py
    Finalizado Tema1 y Tema 2 con sus pruebas respectivas y funcionando

--- Version_B/test_B.py
+++ Version_B/test_B.py
@@ -75,9 +75,47 @@ class Test(unittest.TestCase):
         msg = tema1.premios_depositos_plazo_fijo(plazo, monto)
         self.assertEquals(msg, "Tv LED 32 LG")
 
-    #def test_tema2_prueba_ID(self):
-        # inserte su codigo de prueba
-        # self.assertEquals()
+    def test_tema2_prueba_1(self):
+        tipo_cliente = "AAA"
+        monto = 20000
+
+        msg = tema2.ofrecer_tarjeta_con_chip(monto,tipo_cliente)
+        self.assertEquals(msg, "Tipo de tarjeta: Gold")
+
+    def test_tema2_prueba_2(self):
+        tipo_cliente = "AA"
+        monto = 16000
+
+        msg = tema2.ofrecer_tarjeta_con_chip(monto, tipo_cliente)
+        self.assertEquals(msg, "Tipo de tarjeta: Platinum")
+
+    def test_tema2_prueba_3(self):
+        tipo_cliente = "A"
+        monto = 12000
+
+        msg = tema2.ofrecer_tarjeta_con_chip(monto, tipo_cliente)
+        self.assertEquals(msg, "Tipo de tarjeta: Internacional")
+    def test_tema2_prueba_4(self):
+        tipo_cliente = "B"
+        monto = 6000
+
+        msg = tema2.ofrecer_tarjeta_con_chip(monto, tipo_cliente)
+        self.assertEquals(msg, "Tipo de tarjeta: Club Miles")
+
+    def test_tema2_prueba_5(self):
+        tipo_cliente = "C"
+        monto = 4000
+
+        msg = tema2.ofrecer_tarjeta_con_chip(monto, tipo_cliente)
+        self.assertEquals(msg, "Tipo de tarjeta: Advantage")
+
+    def test_tema2_prueba_6(self):
+        tipo_cliente = "D"
+        monto = 2000
+
+        msg = tema2.ofrecer_tarjeta_con_chip(monto, tipo_cliente)
+        self.assertEquals(msg, "Tipo de tarjeta: Clasica")
+
 
 if __name__ == '__main__':
     unittest.main()

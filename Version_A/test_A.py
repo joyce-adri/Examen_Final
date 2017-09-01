f104e8728bc4f15ed55e76287231df725bc599c4
Julian Adams <jadams@espol.edu.ec>
2017-09-01 08:42:16 -0500

    Tema 1, test 1

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -5,18 +5,23 @@ import unittest
 import tema1
 import tema2
 
+from tema1 import etiquetado_consumo_energia
 class Test(unittest.TestCase):
     
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_tema1_prueba_ID(self):
+    def test_tema1_prueba_1(self):
         # inserte su codigo de prueba
-        self.assertEquals()
+        eti = etiquetado_consumo_energia(20)
+        self.assertEquals(eti, "escala: A++ , eficiencia: los más eficientes")
 
+	#============================================#
+"""
     def test_tema2_prueba_ID(self):
         # inserte su codigo de prueba
         self.assertEquals()
+"""
 
 if __name__ == '__main__':
     unittest.main()

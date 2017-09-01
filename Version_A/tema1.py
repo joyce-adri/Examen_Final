0630edbda5f863cce38abb5fbd44f0fb15606ec5
Juan Alvarado <CltControl@GGIECSOFWRK160.espol.edu.ec>
2017-09-01 09:09:20 -0500

    Add tema 1 test 6, 7, 8, 9

--- Version_A/tema1.py
+++ Version_A/tema1.py
@@ -6,27 +6,27 @@ def etiquetado_consumo_energia(porcentaje):
     rango = "ninguno"
     if porcentaje >= 0 and porcentaje < 90: # [0, 90)
         rango = "los más eficientes"
-        if porcentaje < 30 and porcentaje >= 0 : # [0, 30)
+        if porcentaje < 30 and porcentaje >= 0 : # 1 [0, 30)
             clasificacion_escala_int = "A++"
-        elif porcentaje >= 30 and porcentaje < 42 : # [30, 42)
+        elif porcentaje >= 30 and porcentaje < 42 : # 2 [30, 42)
             clasificacion_escala_int = "A+"
-        elif porcentaje >= 42 and porcentaje < 55 : # [42, 55)
+        elif porcentaje >= 42 and porcentaje < 55 : # 3 [42, 55)
             clasificacion_escala_int = "A"
-        elif porcentaje >= 55 and porcentaje < 75 : # [55, 75)
+        elif porcentaje >= 55 and porcentaje < 75 : # 4 [55, 75)
             clasificacion_escala_int = "B"
-        elif porcentaje >= 75 and porcentaje < 90 : # [75, 90)
+        elif porcentaje >= 75 and porcentaje < 90 : # 5 [75, 90)
             clasificacion_escala_int = "C"
     elif porcentaje >= 90 and porcentaje < 110: # [90, 110)
         rango = "consumo medio"
-        if porcentaje >= 90 and porcentaje < 100 : # [90, 100)
+        if porcentaje >= 90 and porcentaje < 100 : # 6 [90, 100)
             clasificacion_escala_int = "D"
-        elif porcentaje >= 100 and porcentaje < 110 : # [100, 110)
+        elif porcentaje >= 100 and porcentaje < 110 : # 7 [100, 110)
             clasificacion_escala_int = "E"
     elif porcentaje >= 110: # [110, Infinity)
         rango = "alto consumo energético"
-        if porcentaje >= 110 and porcentaje <= 125 : # [110, 125]
+        if porcentaje >= 110 and porcentaje <= 125 : # 8 [110, 125]
             clasificacion_escala_int = "F"
-        elif porcentaje > 125 : # [126, Infinity)
+        elif porcentaje > 125 : # 9 [126, Infinity)
             clasificacion_escala_int = "G"
     etiqueta = "escala: %s , eficiencia: %s" % (clasificacion_escala_int, rango)
     return etiqueta
--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -40,6 +40,30 @@ class Test(unittest.TestCase):
         esperado = "escala: %s , eficiencia: %s" % ("C", "los más eficientes")
         self.assertEquals(etiqueta, esperado)
 
+    def test_tema1_prueba_6(self):
+        # inserte su codigo de prueba
+        etiqueta = tema1.etiquetado_consumo_energia(90)
+        esperado = "escala: %s , eficiencia: %s" % ("D", "consumo medio")
+        self.assertEquals(etiqueta, esperado)
+
+    def test_tema1_prueba_7(self):
+        # inserte su codigo de prueba
+        etiqueta = tema1.etiquetado_consumo_energia(100)
+        esperado = "escala: %s , eficiencia: %s" % ("E", "consumo medio")
+        self.assertEquals(etiqueta, esperado)
+
+    def test_tema1_prueba_8(self):
+        # inserte su codigo de prueba
+        etiqueta = tema1.etiquetado_consumo_energia(110)
+        esperado = "escala: %s , eficiencia: %s" % ("F", "alto consumo energético")
+        self.assertEquals(etiqueta, esperado)
+
+    def test_tema1_prueba_9(self):
+        # inserte su codigo de prueba
+        etiqueta = tema1.etiquetado_consumo_energia(126)
+        esperado = "escala: %s , eficiencia: %s" % ("G", "alto consumo energético")
+        self.assertEquals(etiqueta, esperado)
+
     #def test_tema2_prueba_ID(self):
         # inserte su codigo de prueba
         #self.assertEquals()

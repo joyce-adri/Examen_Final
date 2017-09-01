709f1cdfb49e2cc31eb450c362f803126cb4323e
ivanmosquera <ivan11mosquera@gmail.com>
2017-09-01 09:37:34 -0500

    Caso de prueba 1 Tema1

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from Version_A import *
+
 import unittest
 import tema1
 import tema2
@@ -9,12 +9,18 @@ class Test(unittest.TestCase):
     
     def test_tema1_prueba_1(self):
         porcentaje = 85
-        mensaje = etiquetado_consumo_energia(porcentaje)
+        mensaje = tema1.etiquetado_consumo_energia(porcentaje)
         self.assertEquals(mensaje, "escala: C , eficiencia: los más eficientes")
 
-    def test_tema2_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
+    #def test_tema2_prueba_2(self):
+     #   porcentaje = 105
+      #  mensaje = tema1.etiquetado_consumo_energia(porcentaje)
+       # self.assertEquals(mensaje, "escala: E , eficiencia: consumo medio")
+
+    #def test_tema2_prueba_3(self):
+     #   porcentaje = 130
+      #  mensaje = tema1.etiquetado_consumo_energia(porcentaje)
+       # self.assertEquals(mensaje, "escala: G , eficiencia: alto consumo energético")
 
 if __name__ == '__main__':
     unittest.main()

e8b158d3c2d2b369c2681cfdc33502b7d9274098
joyce-adri <joyce01sm@gmail.com>
2017-09-04 17:03:27 -0500

    tema 1 CP 5

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -33,6 +33,12 @@ class Test(unittest.TestCase):
         etiqueta = tema1.etiquetado_consumo_energia(55)
         esperado = "escala: %s , eficiencia: %s" % ("B", "los más eficientes")
         self.assertEquals(etiqueta, esperado)
+        
+    def test_tema1_prueba_5(self):
+        # inserte su codigo de prueba
+        etiqueta = tema1.etiquetado_consumo_energia(75)
+        esperado = "escala: %s , eficiencia: %s" % ("C", "los más eficientes")
+        self.assertEquals(etiqueta, esperado)
 
 if __name__ == '__main__':
     unittest.main()

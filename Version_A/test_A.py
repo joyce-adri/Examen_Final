7c2d902b0370bb48dcbd332fcf28e26abf1034ba
joyce-adri <joyce01sm@gmail.com>
2017-09-04 16:46:16 -0500

    tema 1 CP 1

--- Version_A/test_A.py
+++ Version_A/test_A.py
@@ -10,13 +10,11 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_tema1_prueba_ID(self):
+    def test_tema1_prueba_1(self):
         # inserte su codigo de prueba
-        self.assertEquals()
-
-    def test_tema2_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
+        etiqueta = tema1.etiquetado_consumo_energia(0)
+        esperado = "escala: %s , eficiencia: %s" % ("A++", "los más eficientes")
+        self.assertEquals(etiqueta, esperado)
 
 if __name__ == '__main__':
     unittest.main()

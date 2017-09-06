997b907d65bcf9be360b33c63937d380b52b903c
joyce-adri <joyce01sm@gmail.com>
2017-09-06 10:04:31 -0500

    VERSION B tema 1 CP 1

--- Version_B/test_B.py
+++ Version_B/test_B.py
@@ -11,9 +11,12 @@ class Test(unittest.TestCase):
 # >>>>>> Estructura para la codificacion de los casos de prueba <<<<<<
 # Reemplazar EL ID por el numero correspondiente del caso de prueba
     
-    def test_tema1_prueba_ID(self):
-        # inserte su codigo de prueba
-        self.assertEquals()
+    def test_tema1_prueba_1(self):
+        plazo = 120
+        montoInversion = 50001
+        solucion = "Tipo de premio: mini nevera electrolux"
+        resp = tema1.premio_por_deposito_a_plazo_fijo(plazo, montoInversion)
+        self.assertEquals(solucion, resp)
 
     def test_tema2_prueba_ID(self):
         # inserte su codigo de prueba
